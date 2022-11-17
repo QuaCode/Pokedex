378968044144fe2c65be4da70115a6dd7288ad5f
Geovanny Alvarez <geovanny0.0lopez@gmail.com>
2022-11-17 15:20:06 -0600

    ✨ New: Pagination added

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Add Prefix
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      // Allow a dto convert the data
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     })
   );
 
